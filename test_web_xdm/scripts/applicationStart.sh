49226c2f3c23a071869138aa484dde138e2d6cd7
dawin <jinheung90@gmail.com>
2022-12-22 11:46:13 +0900

    add actuator

--- test_web_xdm/scripts/applicationStart.sh
+++ test_web_xdm/scripts/applicationStart.sh
@@ -1,3 +1,6 @@
-sudo nohup java -jar /home/centos/deploy.jar /home/centos/nohup.out 2>&1 &
+STDOUT=/home/centos/stdout.log
+STDERR=/home/centos/stderr.log
+
+sudo nohup java -jar /home/centos/deploy.jar 1>>$STDOUT 2>> $STDERR &
 exit 0;
 
