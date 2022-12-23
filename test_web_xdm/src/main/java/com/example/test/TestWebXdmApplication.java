cc177d4e15cee10e3954063a1c61e59775786dde
dawin <jinheung90@gmail.com>
2022-12-23 17:14:25 +0900

    test

--- test_web_xdm/src/main/java/com/example/test/TestWebXdmApplication.java
+++ test_web_xdm/src/main/java/com/example/test/TestWebXdmApplication.java
@@ -2,7 +2,7 @@ package com.example.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//
 @SpringBootApplication
 public class TestWebXdmApplication {
     public static void main(String[] args) {
