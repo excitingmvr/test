a0a275899cf389f56bafbe9e06d82ed67103f8a9
usdev <usdev@naver.com>
2022-12-28 10:23:13 +0900

    pagenation 최종

--- test_core/src/main/java/com/example/test/core/common/constants/Constants.java
+++ test_core/src/main/java/com/example/test/core/common/constants/Constants.java
@@ -4,7 +4,7 @@ package com.example.test.core.common.constants;
 public final class Constants {
 
 //	for paging
-	public static final int ROW_NUM_TO_SHOW = 10;
+	public static final int ROW_NUM_TO_SHOW = 5;
 	public static final int PAGE_NUM_TO_SHOW = 5;
 
 // insert 또는 update 후 페이지 전환 타입: 1:form, 2:list	
